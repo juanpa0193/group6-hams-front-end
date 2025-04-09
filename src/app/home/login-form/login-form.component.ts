495869a9f635b34bc737530bb6e1bb045a39a15a
juanpa0193 <jvill169@fiu.edu>
2025-04-09 18:21:05 -0400

    added user login functionality and current user tracking to user-profile (#7)

--- src/app/home/login-form/login-form.component.ts
+++ src/app/home/login-form/login-form.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink } from '@angular/router';
+import { AuthService} from '../../services/auth.service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'login-form',
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule,
+    NgIf
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
@@ -13,13 +18,40 @@ import {Router, RouterLink } from '@angular/router';
 
 export class LoginFormComponent {
 
-  constructor(private router: Router) {
+  loginForm: FormGroup;
+  loginError = '';
+  loading = false;
 
+  constructor(private router: Router,
+              private authService: AuthService,
+              private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
   }
 
-  btnClick = function() {
-    console.log('clicked');
-  }
+  btnClick() {
+    if (this.loginForm.invalid) {
+      console.log('Login Form Invalid');
+      return;
+    }
 
+    this.loading = true;
+    this.loginError = '';
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login(email, password).subscribe({
+      next: (response) => {
+        console.log('Login Success!', response)
+        this.router.navigate(['/user']);
+      }, error: (error) => {
+        console.error('Login Unsuccessful :(', error);
+        // Show error message
+      }
+    })
+    return false;
+  }
 
 }
